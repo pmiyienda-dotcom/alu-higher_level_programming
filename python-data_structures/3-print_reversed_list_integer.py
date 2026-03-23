a7deba1607b7bfa53ec8b8a8eb62b6a4491c1da9
pmiyienda-dotcom <p.miyienda@alustudent.com>
2026-03-23 02:29:28 -0700

    data structures

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return ""
     else:
-        for num in my_list[ : :-1]:
-            print("{ :d}".format(num))
+        for num in my_list[::-1]:
+            print("{:d}".format(num))
